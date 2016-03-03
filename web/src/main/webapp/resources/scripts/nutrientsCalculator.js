6ea04b035a1c67418b98789d77bcd1a3b01fb666
Vitek <victorandreevi4@gmail.com>
2016-03-03 20:09:23 +0100

    New UI. Started

--- web/src/main/webapp/resources/scripts/nutrientsCalculator.js
+++ web/src/main/webapp/resources/scripts/nutrientsCalculator.js
@@ -1,4 +1,4 @@
-var app = angular.module('nutrientsCalc', ['ui.slider']);
+var app = angular.module('nutrientsCalc', ['ui.slider', 'ui.bootstrap']);
 
 app.controller('nutrientsCalcCtrl', function ($scope, $http, $filter) {
     $scope.selected = {};
@@ -7,28 +7,112 @@ app.controller('nutrientsCalcCtrl', function ($scope, $http, $filter) {
     $scope.intake = 1200;
     $scope.isValidCalorieInput = true;
 
+
+//    TODO
+    //Material
+
+    $scope.myDate = new Date();
+    $scope.minDate = new Date(
+        $scope.myDate.getFullYear(),
+            $scope.myDate.getMonth() - 2,
+        $scope.myDate.getDate());
+    $scope.maxDate = new Date(
+        $scope.myDate.getFullYear(),
+            $scope.myDate.getMonth() + 2,
+        $scope.myDate.getDate());
+    $scope.onlyWeekendsPredicate = function(date) {
+        var day = date.getDay();
+        return day === 0 || day === 6;
+    };
+
+//
+
+    /*  TODO angular datepi
+     $scope.format = 'dd-MMMM-yyyy';
+
+     $scope.today = function() {
+     $scope.dt = new Date();
+     };
+     $scope.today();
+
+     $scope.open = function($event) {
+     $event.preventDefault();
+     $event.stopPropagation();
+
+     $scope.opened = true;
+     };*/
+
     // Fetch data
     $scope.getProductsDataFromServer = function () {
         $http({method: 'GET', url: 'populateFoodItems'}).
             success(function (data, status, headers, config) {
                 $scope.products = data;
                 console.log("FoodItems received successfully");
+//                    $scope.categories = [];
+//                    for (i = 0; i < $scope.products.length; i++) {
+//                        $scope.categories.push($scope.products[i].category);
+////                        console.log($scope.categories[i]);
+//                    }
+//
+//                    $scope.categoriesTree = $scope.categories[0].parent.parent.children;
+//
+////                    $scope.categories1 = _.uniqBy($scope.categories, 'id');
+////                    for (i = 0; i < $scope.categories1.length; i++) {
+////                        console.log($scope.categories1[i]);
+////                    }
             }).
             error(function (data, status, headers, config) {
                 console.error("FoodItems not received successfully. Status:" + status);
             });
     };
 
-    // Filter checked elements
-    $scope.getSelected = function () {
-        $scope.selectedArr = $filter('filter')($scope.products, {checked: true});
-        if ($scope.selectedArr.length == 'undefined' || $scope.selectedArr.length == 0) {
-            $scope.isDisabledButton = true;
-        } else {
-            $scope.isDisabledButton = false;
+    $scope.mealType = "breakfast";
+
+    $scope.breakfastInput = [];
+    $scope.lunchInput = [];
+    $scope.dinnerInput = [];
+    $scope.snakesInput = [];
+    $scope.selectedArr = [];
+
+    $scope.addProductToCurrentMeal = function (product) {
+        if ($scope.mealType == "breakfast") {
+            $scope.breakfastInput.push(product);
+            $scope.breakfastInput = removeDuplicatesFromArray($scope.breakfastInput);
         }
-        return $scope.selectedArr;
+        if ($scope.mealType == "lunch") {
+            $scope.lunchInput.push(product);
+            $scope.lunchInput = removeDuplicatesFromArray($scope.lunchInput);
+        }
+        if ($scope.mealType == "dinner") {
+            $scope.dinnerInput.push(product);
+            $scope.dinnerInput = removeDuplicatesFromArray($scope.dinnerInput);
+        }
+        if ($scope.mealType == "snacks") {
+            $scope.snakesInput.push(product);
+            $scope.snakesInput = removeDuplicatesFromArray($scope.snakesInput);
+        }
+
+        $scope.selectedArr.push(product);
+        $scope.selectedArr = removeDuplicatesFromArray($scope.selectedArr);
     };
+
+    function removeDuplicatesFromArray(a) {
+        return a.reduce(function (p, c) {
+            if (p.indexOf(c) < 0) p.push(c);
+            return p;
+        }, []);
+    }
+
+    // Filter checked elements
+//    $scope.getSelected = function () {
+//        $scope.selectedArr = $filter('filter')($scope.products, {checked: true});
+//        if ($scope.selectedArr.length == 'undefined' || $scope.selectedArr.length == 0) {
+//            $scope.isDisabledButton = true;
+//        } else {
+//            $scope.isDisabledButton = false;
+//        }
+//        return $scope.selectedArr;
+//    };
 
     $scope.position = {
         floor: 0,
@@ -36,6 +120,10 @@ app.controller('nutrientsCalcCtrl', function ($scope, $http, $filter) {
         firstKnob: 40,
         secondKnob: 60
     };
+
+//    $scope.checkParentId = function(id){
+//        $scope.currentParentId = id;
+//    }
 
     $scope.proteinRange = $scope.position.firstKnob;
     $scope.carbohydrateRange = $scope.position.ceiling - $scope.position.secondKnob;
@@ -136,18 +224,18 @@ app.controller('nutrientsCalcCtrl', function ($scope, $http, $filter) {
         $scope.menu = [];
     };
 
-    $scope.removeThisItem = function (selected) {
-        for (var i = 0; i < $scope.selectedArr.length; i++) {
-            if ($scope.selectedArr[i].itemName === selected.itemName) {
-                $scope.selectedArr[i]['checked'] = false;
-                var removedObject = $scope.selectedArr.splice(i, 1);
+    $scope.removeThisItem = function (selected, arr) {
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i].itemName === selected.itemName) {
+//                $scope.selectedArr[i]['checked'] = false;
+                var removedObject =arr.splice(i, 1);
                 removedObject = null;
                 break;
             }
         }
 
-        if ($scope.selectedArr != undefined) {
-            if ($scope.selectedArr.length > 0) {
+        if ($scope.breakfastInput != undefined || $scope.lunchInput != undefined || $scope.dinnerInput != undefined || $scope.snakesInput != undefined) {
+            if ($scope.breakfastInput.length > 0 || $scope.lunchInput.length >0 || $scope.dinnerInput.length > 0 || $scope.snakesInput.length > 0) {
             } else {
                 $scope.isDisabledButton = true;
                 $scope.isClicked = false;
@@ -243,7 +331,7 @@ app.controller('nutrientsCalcCtrl', function ($scope, $http, $filter) {
     }, true);
 
     // Calculate calorie need
-    $scope.sex= {
+    $scope.sex = {
         chosenSex: "",
         options: [
             {name: "male", value: "male"},
@@ -251,7 +339,7 @@ app.controller('nutrientsCalcCtrl', function ($scope, $http, $filter) {
         ]
     };
 
-    $scope.activityLevel= {
+    $scope.activityLevel = {
         chosenLevel: "",
         options: [
             {name: "< 1 hour exercise per week", value: 1.1},
@@ -261,7 +349,7 @@ app.controller('nutrientsCalcCtrl', function ($scope, $http, $filter) {
         ]
     };
 
-    $scope.goal= {
+    $scope.goal = {
         chosenGoal: "",
         options: [
             {name: "Maintain", value: 1},
@@ -273,10 +361,10 @@ app.controller('nutrientsCalcCtrl', function ($scope, $http, $filter) {
     // basal metabolic rate (BMR)
     $scope.calculateBMR = function (sex, age, weight, height) {
         if (sex === "female") {
-           return $scope.BMR = 10 * weight + 6.25 * height - 5 * age - 161;
+            return $scope.BMR = 10 * weight + 6.25 * height - 5 * age - 161;
         }
         if (sex === "male") {
-           return $scope.BMR = 10 * weight + 6.25 * height - 5 * age + 5;
+            return $scope.BMR = 10 * weight + 6.25 * height - 5 * age + 5;
         }
     };
 
@@ -285,19 +373,69 @@ app.controller('nutrientsCalcCtrl', function ($scope, $http, $filter) {
         return $scope.TDEE = BMR * activityCoefficient;
     };
 
-    $scope.$watch('[sex.chosenSex, age, userHeight, userWeight, activityLevel.chosenLevel, goal.chosenGoal]', function (newVal, oldVal) {
-        $scope.BMR =  $scope.calculateBMR($scope.sex.chosenSex.value, $scope.age, $scope.userWeight, $scope.userHeight);
-        $scope.TDEE =  $scope.calculateTDEE($scope.BMR, $scope.activityLevel.chosenLevel.value );
+    $scope.$watch('persDataForm.$valid', function (validity) {
+        $scope.validity = validity;
+        $scope.$watch('[sex.chosenSex, age, userHeight, userWeight, activityLevel.chosenLevel, goal.chosenGoal]', function (newVal, oldVal) {
+            if ($scope.validity) {
+                $scope.BMR = Math.round($scope.calculateBMR($scope.sex.chosenSex.value, $scope.age, $scope.userWeight, $scope.userHeight));
+                $scope.TDEE = Math.round($scope.calculateTDEE($scope.BMR, $scope.activityLevel.chosenLevel.value));
 
-        if($scope.goal.chosenGoal.name === "Cut"){
-           return $scope.intake =  $scope.TDEE + $scope.goal.chosenGoal.value * $scope.TDEE;
-        }
-        if($scope.goal.chosenGoal.name === "Gain"){
-           return $scope.intake =  $scope.TDEE + $scope.goal.chosenGoal.value * $scope.TDEE;
-        } if($scope.goal.chosenGoal.name === "Maintain") {
-            return $scope.intake =  $scope.TDEE;
-        }
-    }, true);
+                if ($scope.goal.chosenGoal.name === "Cut") {
+                    return $scope.intake = Math.round($scope.TDEE + $scope.goal.chosenGoal.value * $scope.TDEE);
+                }
+                if ($scope.goal.chosenGoal.name === "Gain") {
+                    return $scope.intake = Math.round($scope.TDEE + $scope.goal.chosenGoal.value * $scope.TDEE);
+                }
+                if ($scope.goal.chosenGoal.name === "Maintain") {
+                    return $scope.intake = Math.round($scope.TDEE);
+                }
+            }
 
-    // Calculate calorie need END
+        }, true);
+
+    });
+
+    $scope.startsWith = function (actual, expected) {
+        var lowerStr = (actual + "").toLowerCase();
+        return lowerStr.indexOf(expected.toLowerCase()) === 0;
+    };
+
+    $scope.birthDate = '2013-07-23';
+    $scope.dateOptions = {};
 });
+
+//app.directive('datepickerPopup', function (dateFilter, datepickerPopupConfig) {
+//    return {
+//        restrict: 'A',
+//        priority: 1,
+//        require: 'ngModel',
+//        link: function(scope, element, attr, ngModel) {
+//            var dateFormat = attr.datepickerPopup || datepickerPopupConfig.datepickerPopup;
+//            ngModel.$formatters.push(function (value) {
+//                return dateFilter(value, dateFormat);
+//            });
+//        }
+//    };
+//});
+
+//app.directive('customDatepicker',function($compile,$timeout){
+//    return {
+//        replace:true,
+//        templateUrl:'custom-datepicker.html',
+//        scope: {
+//            ngModel: '=',
+//            dateOptions: '@',
+//            dateDisabled: '@',
+//            opened: '=',
+//            min: '@',
+//            max: '@',
+//            popup: '@',
+//            options: '@',
+//            name: '@',
+//            id: '@'
+//        },
+//        link: function($scope, $element, $attrs, $controller){
+//
+//        }
+//    };
+//})
